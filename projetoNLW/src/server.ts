3c211aff15af1d347fec12ec82a731f02339bd6e
iurimega13 <iurimega13@gmail.com>
2021-04-19 15:42:43 -0300

    GET

--- projetoNLW/src/server.ts
+++ projetoNLW/src/server.ts
@@ -1,6 +1,18 @@
-import express from "express";
+import express, { request, response } from "express";
 
 
 const app = express();
+
+/**
+ * GET => Buscas
+ * POST => Criação
+ * PUT => Alteração
+ * DELETE => Deletar
+ * PATCH = Alterar informação especifica
+ */
+
+app.get("/", (request, response) => {
+  return response.send("Olá IURI!!!")
+})
 
 app.listen(3333, () => console.log("Server running in port 3333"));
